ddaa5cb864cadd6daf69e079129c890242f464ac
Daria Ohulchanska <daria.ohulchanska@gmail.com>
2025-02-19 12:25:06 +0100

    init: add solution structure and .gitignore

---
+++ BookMangement.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace BookMangementAPI.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}

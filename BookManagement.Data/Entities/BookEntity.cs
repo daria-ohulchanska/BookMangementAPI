f3dd1f3adf92023261856c64adb5fb71deb55015
Daria Ohulchanska <daria.ohulchanska@gmail.com>
2025-02-23 17:15:22 +0100

    remove Rating field from BookEntity

--- BookManagement.Data/Entities/BookEntity.cs
+++ BookManagement.Data/Entities/BookEntity.cs
@@ -5,6 +5,5 @@ public class BookEntity : Entity
     public string Title { get; set; } = string.Empty;
     public string Author { get; set; } = string.Empty;
     public int ViewsCount { get; set; }
-    public int Rating { get; set; }
     public int PublicationYear { get; set; }
 }
